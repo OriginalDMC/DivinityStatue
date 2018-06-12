7678652a564ff94b9facce6eaa3384d4927ff076
OriginalDMC <38869965+OriginalDMC@users.noreply.github.com>
2018-06-13 04:48:16 +0500

    Add files via upload

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1002,4 +1002,4 @@
         }
     }
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobiri.se">Mobirise</a> Mobirise v4.7.2';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobiri.se">Mobirise</a> Mobirise v4.7.7';document.body.insertBefore(a,document.body.childNodes[0])}}();
