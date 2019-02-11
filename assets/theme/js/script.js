bd68d289bda358dc7de217e0083b969637ef853d
OriginalDMC <38869965+OriginalDMC@users.noreply.github.com>
2019-02-12 02:41:25 +0500

    Add files via upload

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -701,7 +701,7 @@
                 var videoIframe = $(item).parents('section').find('iframe')[0],
                     videoIframeSrc = $(videoIframe).attr('src');
 
-                item.parents('section').css('z-index', '1');
+                item.parents('section').css('z-index', '5000');
 
                 if (videoIframeSrc.indexOf('youtu') !== -1) {
                     videoIframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
@@ -1002,4 +1002,4 @@
         }
     }
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.8.10';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.9.3';document.body.insertBefore(a,document.body.childNodes[0])}}();
